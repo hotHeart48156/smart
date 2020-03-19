c9df5f99f1f743626f670917abed67df13db0e9d
popo <000111aaaim@gmail.com>
2020-03-20 00:40:38 +0800

    add
    Signed-off-by: popo <000111aaaim@gmail.com>

--- activite/activite-app/src/main/java/org/activite/commandhandle/AbstractCommandHandle.java
+++ activite/activite-app/src/main/java/org/activite/commandhandle/AbstractCommandHandle.java
@@ -1,11 +1,9 @@
 package org.activite.commandhandle;
 
 /**
-import org.activite.executor.command.*;
-import org.springframework.stereotype.Component;
 @author yangbiao
  */
 
-@Component
+
 public abstract class AbstractCommandHandle  {
 }
--- users/users-controller/src/main/java/org/users/controller/UserController.java
+++ users/users-controller/src/main/java/org/users/controller/UserController.java
@@ -5,7 +5,6 @@ import org.axonframework.commandhandling.CommandExecutionException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.users.service.UserApplicationImpl;
 
 /**
  * @author "yangbiao"
