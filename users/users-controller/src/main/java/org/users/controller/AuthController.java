37ccefa30c3a16fa8d5dfed470964816b6554dfe
OMEN <2680323775@qq.com>
2020-04-03 23:45:48 +0800

    add

---
+++ users/users-controller/src/main/java/org/users/controller/AuthController.java
@@ -0,0 +1,11 @@
+package org.users.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author "yangbiao"
+ */
+@RestController("/auth")
+public class AuthController {
+
+}
